d8e83f2ac42952f1a5cba85a9d386fd11f75ba46
Diego Fernando Bolivar <diegofernando83@gmail.com>
2012-07-05 15:53:42 -0500

    cambia a base1

--- website/views.py
+++ website/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 def home(request):
-	return render_to_response('base.html', context_instance=RequestContext(request))
+	return render_to_response('base1.html', context_instance=RequestContext(request))
